a74edf3885d3cbbfe270ab75bac939627e481768
rasceta <rio.sufilin@gmail.com>
2020-07-16 19:11:24 +0700

    fixed token description at shop and exchange

--- main.py
+++ main.py
@@ -778,12 +778,12 @@ async def shop(ctx):
     embed = discord.Embed(  title="Apollo's Shop",
                             description="Welcome to Apollo's Shop! You can purchase any of these items by typing `!apollo purchase <item>`",
                             color=discord.Color.green())
-    embed.add_field(name="**🍲Soup Kettle Token🍲**. Can be traded for a soup Kettle. Which you can then turn in the soup kettle for bells. Each soup kettle when given to a treasurer is worth 99,000 Bells", value="Price: 💰4.000", inline=False)
-    embed.add_field(name="**🔴Foundation Token🔴**. Worth 1 stack of anything from the dodo code", value="Price: 💰12.000", inline=True)
-    embed.add_field(name="**♥Heart Token♥**. Worth 3 stacks of anything from the dodo code", value="Price: 💰20.000")
-    embed.add_field(name="**💖Love Token💖**. By buying this token you are entitled to a 15 stacks of items randomly chosen from our 'free stuff' category!", value="Price: 💰100.000", inline=False)
-    embed.add_field(name="**🎲Gambler🎲**. Purchasing this role signifies your dedication as a gambler, and not only do you have the fiery spirit of one, but you also have the coins to back it up!", value="Price: 💰20.000", inline=False)
-    embed.add_field(name="**🤑Wealthy🤑**. Purchasing this role means you have an ample amount of coins at your disposal! The role is permanent and everyone can see exactly how wealthy you are.", value="Price: 💰200.000", inline=True)
+    embed.add_field(name="**🍲Soup Kettle Token🍲**. Can be traded for a soup Kettle. Which you can then turn in the soup kettle for bells. Each soup kettle when given to a treasurer is worth 99,000 Bells", value="Price: 💰4,000", inline=False)
+    embed.add_field(name="**🔴Foundation Token🔴**. Worth 1 random stack from the dodo code", value="Price: 💰12,000", inline=True)
+    embed.add_field(name="**♥Heart Token♥**. Worth 3 random stacks from the dodo code", value="Price: 💰20,000")
+    embed.add_field(name="**💖Love Token💖**. Worth 15 random stacks from the dodo code", value="Price: 💰100,000", inline=False)
+    embed.add_field(name="**🎲Gambler🎲**. Purchasing this role signifies your dedication as a gambler, and not only do you have the fiery spirit of one, but you also have the coins to back it up!", value="Price: 💰20,000", inline=False)
+    embed.add_field(name="**🤑Wealthy🤑**. Purchasing this role means you have an ample amount of coins at your disposal! The role is permanent and everyone can see exactly how wealthy you are.", value="Price: 💰200,000", inline=True)
     embed.add_field(name="Notes",value="If you want to turn in your items! Please use `!apollo exchange <item>, <dodo code>`. **Please be sure to open your island before turning in a token!**",inline=False)
 
     await ctx.send(embed=embed)
@@ -860,8 +860,8 @@ async def exchange(ctx, *, item : str):
             exchanged_item = splitted_words[0].lower()
             exchanged_note = splitted_words[1]
             item = item.lower()
-            items_dict = {'soup kettle token':'99,000 Bells', 'foundation token':'1 stack of anything', 
-                        'heart token':'3 stacks of anything', 'love token':'15 stacks of anything'}
+            items_dict = {'soup kettle token':'99,000 Bells', 'foundation token':'1 random stack', 
+                        'heart token':'3 random stacks', 'love token':'15 random stacks'}
 
     
             cursor = CONN.cursor()
